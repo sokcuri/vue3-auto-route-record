aac1f5daa5182b6e44325095e770bec055f82f5d
sokcuri <sokcuri@gmail.com>
2020-09-28 16:00:53 +0900

    implements simple auto route

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,9 +1,92 @@
-import DemoPlugin from './main';
+import Vue3AutoRouteRecord from './main'
+import * as route from './main'
 
 test('should set options property', () => {
   const opts = {
-    test: 'hello',
-  };
-  const plugin = new DemoPlugin(opts);
-  expect(plugin.options).toBe(opts);
-});
+    baseDir: './src/',
+    outputDir: './dist'
+  }
+
+  const plugin = new Vue3AutoRouteRecord(opts);
+
+  expect(plugin.options).toEqual(
+    expect.objectContaining(opts)
+  )
+})
+
+test('glob test', () => {
+  const expected = ['main.test.ts', 'main.ts']
+  const files = route.getRoutableFiles('src', 'ts')
+
+  expect(expected).toEqual(files)
+})
+
+test('validate route config', () => {
+  const files = [
+    'index.vue',
+    'about.vue',
+    'user/one.vue',
+  ];
+
+  const expected = [{
+    real: 'index.vue',
+    name: 'index',
+    path: '/',
+  },
+  {
+    real: 'about.vue',
+    name: 'about',
+    path: '/about'
+  },
+  {
+    real: 'user/one.vue',
+    name: 'user-one',
+    path: '/user/one'
+  }]
+
+  const routeConfig = route.getRouteObjects(files);
+  expect(JSON.stringify(expected)).toEqual(JSON.stringify(routeConfig))
+})
+
+test('make route string', () => {
+
+  const data = [{
+    real: 'index.vue',
+    name: 'index',
+    path: '/',
+  },
+  {
+    real: 'about.vue',
+    name: 'about',
+    path: '/about'
+  },
+  {
+    real: 'user/one.vue',
+    name: 'user-one',
+    path: '/user/one'
+  }]
+
+  const expected =
+`const routes = [
+  {
+    component: () => import(/* webpackChunkName: "pf-index" */ '@/views/index.vue'),
+    name: 'index',
+    path: '/'
+  },
+  {
+    component: () => import(/* webpackChunkName: "pf-about" */ '@/views/about.vue'),
+    name: 'about',
+    path: '/about'
+  },
+  {
+    component: () => import(/* webpackChunkName: "pf-user-one" */ '@/views/user/one.vue'),
+    name: 'user-one',
+    path: '/user/one'
+  }
+]
+export default routes`
+
+  const chunkPrefix = 'pf-'
+  const outputPrefix = '@/views/'
+  expect(expected).toEqual(route.makeRouteString(data, chunkPrefix, outputPrefix));
+})
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,86 @@
 import { DefinePlugin, Compiler } from 'webpack';
+import path from 'path';
+import fg from 'fast-glob'
+import fs from 'fs-extra';
 
 type DefinitionKeyValue = {[key: string]: DefinePlugin.CodeValueObject};
 
-export default class DemoPlugin extends DefinePlugin  {
+interface Options {
+  baseDir: string;
+  chunkPrefix?: string;
+  importPrefix?: string;
+  outputDir: string;
+}
+
+export default class Vue3AutoRouteRecord extends DefinePlugin {
+  options: Options;
+
   /**
    *
    */
-  constructor(definitions: DefinitionKeyValue) {
-    super(definitions);
-    this.options = definitions;
-  }
+  constructor(options: Options) {
+    super(options as unknown as DefinitionKeyValue);
+    this.options = options;
 
-  options: {};
+    if (typeof options.baseDir !== 'string') throw new Error('baseDir parameter required')
+    if (typeof options.outputDir !== 'string') throw new Error('outputDir parameter required')
+  }
 
   apply(compiler: Compiler) {
-    compiler.hooks.done.tap('DemoPlugin', () => {
-      console.log('\nHello world in typescript\n');
-    });
+    const chunkPrefix = this.options.chunkPrefix || ''
+    const importPrefix = this.options.importPrefix || ''
+
+    compiler.hooks.entryOption.tap('Vue3AutoRouteRecord', () => {
+      const files = getRoutableFiles(this.options.baseDir);
+      const obj = getRouteObjects(files);
+      const routes = makeRouteString(obj, chunkPrefix, importPrefix);
+
+      const outputDir = this.options.outputDir;
+      fs.outputFileSync(path.join(outputDir, 'routes.ts'), routes);
+    })
   }
+}
+
+export interface RouteObject {
+  real: string;
+  path: string;
+  name: string;
+}
+
+export function getRoutableFiles(baseDir: string, ext: string = 'vue') {
+  const relativePath = path.join(path.relative('.', baseDir), '/');
+  return fg.sync(path.join(baseDir, '**', '*.' + ext), { unique: true })
+    .filter(x => x.indexOf(relativePath) === 0)
+    .map(x => x.replace(relativePath, ''));
+}
+
+export function getRouteObjects(files: string[]) {
+  return files.map(file => {
+    let path;
+
+    let filename = file.substr(0, file.lastIndexOf('.'));
+    const name = filename.replace(/\//g, '-');
+    const real = file;
+
+    if (/index$/.test(filename.toLowerCase())) {
+      path = '/' + filename.substr(0, filename.lastIndexOf('/'));
+    } else {
+      path = '/' + filename;
+    }
+
+    path = path.toLowerCase()
+
+    return { real, name, path }
+  })
+}
+
+export function makeRouteString(routeObject: RouteObject[], chunkPrefix: string, importPrefix: string) {
+  const routes = routeObject.map(x =>
+`  {
+    component: () => import(/* webpackChunkName: "${chunkPrefix}${x.name}" */ '${importPrefix}${x.real}'),
+    name: '${x.name}',
+    path: '${x.path}'
+  }`).join(',\n')
+
+  return `const routes = [\n${routes}\n]\nexport default routes`
 }
